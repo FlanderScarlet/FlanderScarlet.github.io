ba2365585bd4dffd8e7828be4ecb5cb5233b8a17
IceFrogs <icefrogs@foxmail.com>
2024-04-01 00:34:46 +0800

    first commit

--- .vuepress/config.ts
+++ .vuepress/config.ts
@@ -29,8 +29,8 @@ export default defineUserConfig({
     // },
     navbar: [
       { text: "主页", link: "/" },
-      { text: "分类", link: "/categories/biji/1/" },
-      { text: "标签", link: "/tags/Linux/1/" },
+      { text: "分类", link: "/categories/stm32/1/" },
+      { text: "标签", link: "/tags/Git/1/" },
       // {
       //   text: "Docs",
       //   children: [
---
+++ blogs/stm32/test.md
@@ -0,0 +1,13 @@
+---
+title: 配置GitHub SSH默认使用443端口
+date: 2024/03/26
+tags:
+ - Git
+ - GitHub
+ - SSH
+categories:
+ - stm32
+---
+
+# 
+##
